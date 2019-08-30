4828585922c2b8d0cf1e624a3d4f1e103df4fc47
Kirti <kirtikumar42@gmail.com>
2019-08-30 17:34:34 +0530

    my second commit.

--- meeting/src/main/java/com/meeting/booking/config/MeetingApplication.java
+++ meeting/src/main/java/com/meeting/booking/config/MeetingApplication.java
@@ -1,14 +1,21 @@
-package com.meeting.booking.config;
+ package com.meeting.booking.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootExceptionReporter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.meeting.booking")
 @EntityScan(basePackages = { "com.meeting.booking.model" })
-public class MeetingApplication {
+public class MeetingApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MeetingApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetingApplication.class, args);
--- meeting/src/main/java/com/meeting/booking/service/BookingServiceImpl.java
+++ meeting/src/main/java/com/meeting/booking/service/BookingServiceImpl.java
@@ -4,7 +4,22 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
+
+import javax.activation.DataHandler;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -72,14 +87,16 @@ public class BookingServiceImpl implements BookingService {
 		Integer formateDate2;
 		Integer startMinute;
 		Integer endMinute;
+		String[] splitStartTime;
+		String[] splitEndTime;
 		try {
 			String []splitStartDate =bookingDetails.getStartTime().split(" ");
 			String []splitEndDate =bookingDetails.getEndTime().split(" ");
 			
 			String []SplitStartDate1=splitStartDate[0].split("/");
 			String []SplitEndDate2=splitEndDate[0].split("/");
-			String[] splitStartTime=splitStartDate[1].split(":");
-			String[] splitEndTime=splitEndDate[1].split(":");
+			splitStartTime=splitStartDate[1].split(":");
+			splitEndTime=splitEndDate[1].split(":");
 
 		 startMinute=(Integer.parseInt(splitStartTime[0])*60)+Integer.parseInt(splitStartTime[1]);
 		 endMinute=(Integer.parseInt(splitEndTime[0])*60)+Integer.parseInt(splitEndTime[1]);
@@ -122,8 +139,15 @@ public class BookingServiceImpl implements BookingService {
 		booking.setNewBooking(newBooking);
 		newBooking.setBooking(booking);
 		bookingDao.save(booking,null);
+		
 		try {
-			mailService.sendBookingConfirmationMail(user, meetingRoom, booking);
+			String st=formateDate1+"T"+splitStartTime[0]+""+splitStartTime[1]+"00";
+			String et=formateDate2+"T"+splitEndTime[0]+""+splitEndTime[1]+"00";
+			String email=user.getEmailId();
+			String purpose=booking.getPurpose();
+			String roomName=meetingRoom.getRoomName();
+			BookingServiceImpl.send(st,et,email,purpose,roomName);
+			//mailService.sendBookingConfirmationMail(user, meetingRoom, booking);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -176,5 +200,124 @@ public class BookingServiceImpl implements BookingService {
 		// TODO Auto-generated method stub
 		return bookingDao.deleteById(id);
 	}
+	
+	// create an event in mail
+	public static void send(String st, String et, String email, String purpose, String roomName) throws Exception {
+
+	  try {
+	      String from = "bookmeeting6@gmail.com";
+	      String to =email;
+	      Properties prop = new Properties();
+
+	      prop.put("mail.smtp.auth", "true");
+	      prop.put("mail.smtp.starttls.enable", "true");
+	      prop.put("mail.smtp.host", "smtp.gmail.com");
+	      prop.put("mail.smtp.port", "587");
+
+	      final String username = "bookmeeting6@gmail.com";
+	      final String password = "Rahul@95";
+
+	      Session session = Session.getInstance(prop,
+	                new javax.mail.Authenticator() {
+	                  protected PasswordAuthentication getPasswordAuthentication() {
+	                      return new PasswordAuthentication(username, password);
+	                  }
+	                });
+
+	      // Define message
+	      MimeMessage message = new MimeMessage(session);
+	      message.addHeaderLine("method=REQUEST");
+	      message.addHeaderLine("charset=UTF-8");
+	      message.addHeaderLine("component=VEVENT");
+
+	      message.setFrom(new InternetAddress(from));
+	      message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	      message.setSubject("Outlook Meeting Request Using JavaMail");
+
+	      StringBuffer sb = new StringBuffer();
+
+	      StringBuffer buffer = sb.append(
+	              "BEGIN:VCALENDAR\n"
+	              + "PRODID:-//Microsoft Corporation//Outlook 9.0 MIMEDIR//EN\n"
+	              + "VERSION:2.0\n"
+	              + "METHOD:REQUEST\n"
+	              + "BEGIN:VTIMEZONE\n"
+	              + "TZID:America/New_York\n"
+	              + "X-LIC-LOCATION:America/New_York\n"
+	              + "BEGIN:STANDARD\n"
+	             // + "DTSTART:20071104T020000\n"
+	       //       + "TZOFFSETFROM:-0400\n"
+	     //         + "TZOFFSETTO:-0500\n"
+	              + "TZNAME:EST\n"
+	              + "END:STANDARD\n"
+	              + "BEGIN:DAYLIGHT\n"
+	           //   + "DTSTART:20070311T020000\n"
+	         //     + "TZOFFSETFROM:-0500\n"
+	            //  + "TZOFFSETTO:-0400\n"
+	              + "TZNAME:EDT\n"
+	              + "END:DAYLIGHT\n"
+	              + "END:VTIMEZONE\n"
+	              + "BEGIN:VEVENT\n"
+	              +"TRANSP:OPAQUE\n"
+	             +"CREATED:"+st+"\n"
+	             +"LAST-MODIFIED:"+st+"\n"
+	              +"ATTENDEE;ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:"+email+"\n"
+	              + "ORGANIZER:MAILTO:hockeyonicethricewastoldcold?.com\n"
+	              + "DTSTART;TZID=Asia/Kolkata:"+st+"\n"
+	          //    + "DTEND;TZID=Asia/Kolkata:"+et+"\n"
+	             +"RRULE:FREQ=DAILY;BYDAY=MO,TU,WE,TH,FR,SA,SU;UNTIL="+et+"\n"
+	         //   +"RRULE:FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1;COUNT=1\n"
+	           //   +"RRULE:FREQ=DAILY;INTERVAL=2;BYDAY=MO,TU,WE,TH,FR;BYHOUR=10,11\n"
+	              + "LOCATION:"+roomName+"\n"
+	              + "TRANSP:OPAQUE\n"
+	              + "SEQUENCE:0\n"
+	           //   + "UID:040000008200E00074C5B7101A82E00800000000002FF466CE3AC5010000000000000000100\n"
+	              + "DTSTAMP:"+st+"\n"
+	              + "CATEGORIES:Meeting\n"
+	              + "DESCRIPTION:"+purpose+"\n"
+	              + "SUMMARY:Your Confrence Room has Been Booked\n"
+	              + "PRIORITY:5\n"
+	              + "CLASS:PUBLIC\n"
+	              + "BEGIN:VALARM\n"
+	              + "TRIGGER:PT1440M\n"
+	              + "ACTION:DISPLAY\n"
+	              + "DESCRIPTION:Reminder\n"
+	              + "END:VALARM\n"
+	              + "END:VEVENT\n"
+	              + "END:VCALENDAR");
+
+	      
+	      // Create the message part
+	      BodyPart messageBodyPart = new MimeBodyPart();
+
+	      // Fill the message
+	      messageBodyPart.setHeader("Content-Class", "urn:content-  classes:calendarmessage");
+	      messageBodyPart.setHeader("Content-ID", "calendar_message");
+	      messageBodyPart.setDataHandler(new DataHandler(
+	              new ByteArrayDataSource(buffer.toString(), "text/calendar")));// very important
+
+	      // Create a Multipart
+	      Multipart multipart = new MimeMultipart();
+
+	      // Add part one
+	      multipart.addBodyPart(messageBodyPart);
+
+	      // Put parts in message
+	      message.setContent(multipart);
+
+	      // send message
+
+
+	      Transport.send(message);
+
+	      System.out.println("Success");
+
+	  } catch (MessagingException me) {
+	      me.printStackTrace();
+	  } catch (Exception ex) {
+	      ex.printStackTrace();
+	  }
+	}
+
 
 }
